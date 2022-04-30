e4600d8e4427c95445a03164e6facdb1ee8bb086
Rither <rither5711@gmail.com>
2022-04-30 17:20:18 -0500

    se termino el fomulario

--- app.js
+++ app.js
@@ -21,3 +21,37 @@ menu.addEventListener("click",(e)=>{
     }
 
 })
+
+/* 
+formulario de contacto */
+
+const $formulario = document.querySelector(".contact-form")
+const $loader =  document.querySelector(".contact-form-loader")
+const $response = document.querySelector(".contact-form-respose")
+
+$formulario.addEventListener("submit",e =>{
+    e.preventDefault()
+    $loader.classList.remove("none")
+    fetch("https://formsubmit.co/ajax/rither5711@gmail.com",{method:"POST",
+    body:new FormData(e.target)
+    })
+    .then(res =>(res.ok ? res.json():Promise.reject(res)))
+    .then(json=>{
+            console.log(json)
+            $loader.classList.add("none")
+            location.hash = "#gracias"
+            $formulario.reset()
+    })
+    .catch(err =>{
+        console.log(err)
+        let mesaage = err.statusTxt || "Ocurrio un error al enviar intentalo nuevamente"
+        $response.querySelector("h3").innerHTML = `Error ${err.status}: ${mesaage}`
+    })
+    .finally(()=>{
+        $loader.classList.add("none")
+        setTimeout(()=>{
+            location.hash = "#close"
+        },3000)
+    })
+})
+
