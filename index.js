1ddcc3b5b4709fa6ee86b57ed6e5d25346e40798
Rither <rither5711@gmail.com>
2022-05-01 00:30:50 -0500

    Uso del scroll reveal

---
+++ index.js
@@ -0,0 +1,87 @@
+window.sr = ScrollReveal();
+sr.reveal(".logo",{
+    duration:2000,
+    origin: 'right',
+    distance:'-100px'
+});
+
+
+/* titulo */
+
+sr.reveal(".hero-image-blur",{
+    duration:2000,
+    origin: 'left',
+    distance:'200px'
+});
+
+
+sr.reveal(".hero-image-tittle",{
+    duration:2000,
+    origin: '',
+    distance:'200px',
+});
+
+sr.reveal(".hero-image-subtittle",{
+    duration:2000,
+    origin: 'left',
+    distance:'200px',
+    delay: 1000
+});
+
+
+/* about */
+sr.reveal(".text-center ",{
+    duration:2000,
+    distance:'-100px',
+    origin: 'left'
+});
+
+sr.reveal(".descripcion",{
+    duration:2000,
+    distance:'200px',
+});
+
+sr.reveal(".grid-layout",{
+    duration:2000,
+    distance:'200px',
+});
+
+
+
+
+/* servicios */
+sr.reveal(".card",{
+    duration:2000,
+    distance:'200px',
+});
+
+
+
+/* proyectos */
+
+sr.reveal(".portafolio-card",{
+    duration:2000,
+    origin:'left',
+    distance:'-200px',
+});
+
+
+
+/* formulario */
+
+sr.reveal(".contact-cards",{
+    duration:2000,
+    origin: 'left',
+    distance:'400px',
+});
+
+sr.reveal(".contact-form",{
+    duration:2000,
+    origin: 'right',
+    distance:'400px',
+    delay: 1000
+});
+
+
+
+
