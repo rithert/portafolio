554a137213e84def3301ac99b607e22cf99cb191
Rither <rither5711@gmail.com>
2022-05-05 17:30:59 -0500

    Se solciono tamaño del menu

--- js/index.js
+++ js/index.js
@@ -1,87 +1,86 @@
-window.sr = ScrollReveal();
-sr.reveal(".logo",{
-    duration:2000,
-    origin: 'right',
-    distance:'-100px'
-});
 
+    window.sr = ScrollReveal();
 
-/* titulo */
+    /* titulo */
 
-sr.reveal(".hero-image-blur",{
-    duration:2000,
-    origin: 'left',
-    distance:'200px'
-});
+    sr.reveal(".logo",{
+        duration:2000,
+        origin: 'left',
+        distance:'100px'
+    });
 
+    sr.reveal(".hero-image-blur",{
+        duration:1000,
+        origin: 'left',
+        distance:'100px'
+    });
 
-sr.reveal(".hero-image-tittle",{
-    duration:2000,
-    origin: '',
-    distance:'200px',
-});
+    sr.reveal(".hero-image-tittle",{
+        duration:1000,
+        origin: 'top',
+        distance:'200px',
+    });
+    
+    sr.reveal(".hero-image-subtittle",{
+        duration:1000,
+        origin: 'top',
+        distance:'200px',
+        delay: 1000
+    });
+    
+    
+    //about 
+    sr.reveal(".text-center ",{
+        duration:2000,
+        distance:'100px',
+        origin: 'left'
+    });
+    
 
-sr.reveal(".hero-image-subtittle",{
-    duration:2000,
-    origin: 'left',
-    distance:'200px',
-    delay: 1000
-});
+    sr.reveal(".descripcion",{
+        duration:2000,
+        distance:'200px',
+    });
+    
+    sr.reveal(".grid-layout",{
+        duration:1500,
+        distance:'200px',
+    });
+    
+    
+    
+    //servicios
+    sr.reveal(".card",{
+        duration:3000,
+        distance:'100px',
+    });
+    
+    
+    
+    // proyectos 
+    
+    sr.reveal(".portafolio-card",{
+        duration:2000,
+        origin:'left',
+        distance:'200px',
+    });
+ 
 
+    //formulario
 
-/* about */
-sr.reveal(".text-center ",{
-    duration:2000,
-    distance:'-100px',
-    origin: 'left'
-});
+    sr.reveal(".contact-cards",{
+        duration:2000,
+        origin: 'left',
+        distance:'-10px',
+        delay:1500,
+    });
 
-sr.reveal(".descripcion",{
-    duration:2000,
-    distance:'200px',
-});
-
-sr.reveal(".grid-layout",{
-    duration:2000,
-    distance:'200px',
-});
-
-
-
-
-/* servicios */
-sr.reveal(".card",{
-    duration:2000,
-    distance:'200px',
-});
-
-
-
-/* proyectos */
-
-sr.reveal(".portafolio-card",{
-    duration:2000,
-    origin:'left',
-    distance:'-200px',
-});
-
-
-
-/* formulario */
-
-sr.reveal(".contact-cards",{
-    duration:2000,
-    origin: 'left',
-    distance:'400px',
-});
-
-sr.reveal(".contact-form",{
-    duration:2000,
-    origin: 'right',
-    distance:'400px',
-    delay: 1000
-});
-
+    sr.reveal(".contact-form",{
+        duration:2000,
+        origin: 'right',
+        distance:'-10px',
+        delay:1500,
+    });
 
 
 
